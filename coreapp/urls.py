ce3896214f51a7025f08cde8079a01489d43209c
Tarang <tarang.panchal@biztechcs.com>
2022-05-23 15:43:21 +0530

    added : auth

--- coreapp/urls.py
+++ coreapp/urls.py
@@ -5,5 +5,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='home'),
+
+    ### AUTHENTICATION
+    path('login/', views.handleSignin, name='BlogLogin'),
+    path('register/', views.handleSignup, name='BlogRegister'),
+    path('logout/', views.handleLogout, name='BlogLogout'),
+    ### PRODUCT RELATED
     path('<slug:slug>/', views.productDetail.as_view(), name='productDetail'),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- coreapp/views.py
+++ coreapp/views.py
@@ -1,7 +1,89 @@
+from django.contrib import messages
 from django.shortcuts import render
 from .models import Product, ProductImage
+from django.shortcuts import render, redirect
+from django.template.loader import get_template
 from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.models import User, auth
 from django.views.generic.detail import DetailView
+from django.core.mail import EmailMultiAlternatives
+
+def handleSignin(request):
+    if request.user.is_authenticated:
+        redirect('/blog')
+
+    if request.method=='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if username == '':
+            messages.error(request,'Please enter username!')
+            return render(request, 'login.html')
+        if password == '':
+            messages.error(request,'Please enter password!')
+            return render(request, 'login.html')
+        else:
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('/')
+            else:
+                messages.error(request,'Incorrect username or password!')
+                return render(request, 'coreapp/authentication/login.html')
+    else:
+        return render(request, 'coreapp/authentication/login.html')
+
+def handleLogout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def handleSignup(request):
+    if request.method=='POST':
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+
+        if len(email)<10:
+            messages.error(request, " Your email is not valid")
+            return render(request, 'coreapp/authentication/register.html')
+
+        if len(fname)<2  or len(lname)<2:
+            messages.error(request, " First,Last Name Should be proper")
+            return render(request, 'coreapp/authentication/register.html')
+
+        if (pass1!= pass2):
+             messages.error(request, " Passwords do not match")
+             return render(request, 'coreapp/authentication/register.html')
+
+        if not User.objects.filter(username=email, email=email ).exists():
+            myuser = User.objects.create_user(first_name=fname, last_name=lname, username=email, email=email, password=pass1)
+            #########SEND EMAIL CODE#########
+            plaintext = get_template('email_register.html')
+            htmly     = get_template('email_register.html')
+            baseurl = request.build_absolute_uri()
+            d = { 
+                'first_name': fname,
+                'last_name': lname,
+                'username': email,
+                'password': pass1,
+                'baseurl': baseurl,
+            }
+            subject, from_email, to = 'Your Registration is successful', 'admin@webblogapp.com', email
+            text_content = plaintext.render(d)
+            html_content = htmly.render(d)
+            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+            myuser.save()
+            messages.success(request, "You are Registered successfully !")
+            return redirect('/login')
+        else:
+            messages.error(request, "Whoops .! Username/Email Already in use .!")
+            return render(request, 'coreapp/authentication/register.html')
+    else:
+        return render(request, 'coreapp/authentication/register.html')
 
 # Create your views here.
 def index(request):
